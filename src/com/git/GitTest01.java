ad8b699c5ce65ebb473c342f43a71d06268ecc20
ZhiHong-Wen <2563603624@qq.com>
2021-05-19 08:49:16 +0800

    我修改了，再次将项目提交到远程仓库

--- src/com/git/GitTest01.java
+++ src/com/git/GitTest01.java
@@ -4,5 +4,6 @@ public class GitTest01 {
     public static void main(String[] args) {
         System.out.println("正在测试GitTest01");
         System.out.println("正在修改GitTest01！！！");
+        System.out.println("再次被修改GitTest01！！！");
     }
 }
